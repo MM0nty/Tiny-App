8d821d08cd14eddfb0844b96343493fbb38ebd57
M <79332342+MM0nty@users.noreply.github.com>
2021-08-03 17:50:18 +0000

    Added longURL route

--- ExpressServer.js
+++ ExpressServer.js
@@ -1,6 +1,20 @@
 const express = require("express");
 const app = express();
 const PORT = 8080; //8080 is default port
+const bodyParser = require("body-parser");
+
+function randomString()
+{
+
+}
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.post("/URLs", (request,response) =>
+{
+  console.log(request.body);
+  response.send("Ok");
+});
 
 app.set("view engine", "ejs");
 
@@ -26,10 +40,20 @@ app.get("/hello", (request, response) =>
   response.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
+app.get("/URLs/New", (request, response) =>
+{
+  response.render("URLs_New");
+});
+
 app.get("/urls/:shortURL", (request, response) =>
 {
   const template = { shortURL: request.params.shortURL, longURL: request.params.longURL };
   response.render("Show", template);
+});
+
+app.get("URLs/:shortURL", (request, response) =>
+{
+  response.redirect(longURL);
 });
 
 app.listen(PORT, () =>
