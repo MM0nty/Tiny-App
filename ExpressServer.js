326ce1d7fbb87522ae823baad041270174345079
M <79332342+MM0nty@users.noreply.github.com>
2021-08-03 01:23:22 +0000

    Added hello response page

--- ExpressServer.js
+++ ExpressServer.js
@@ -18,6 +18,11 @@ app.get("/urls.json", (request, response) =>
   response.json(database);
 });
 
+app.get("/hello", (request, response) =>
+{
+  response.send("<html><body>Hello <b>World</b></body></html>\n");
+});
+
 app.listen(PORT, () =>
 {
   console.log(`Listening on port ${PORT}!`);
