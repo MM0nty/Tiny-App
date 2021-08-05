3110e4fce52620084d366a9f81265d37a51b3757
M <79332342+MM0nty@users.noreply.github.com>
2021-08-05 20:53:07 +0000

    Restarting (commenting out everything and slowly uncommenting as I review), featuring more commits

--- ExpressServer.js
+++ ExpressServer.js
@@ -4,127 +4,139 @@ const PORT = 8080; //8080 is default port
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
 
-function randomString()
-{
 
-}
-
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 app.set("view engine", "ejs");
 
-const database =
+const urlDatabase =
 {
   "b2xVn2": "https://www.lighthouselabs.ca",
   "9sm5xK": "https://www.google.com"
 };
 
 const users =
-{ 
-  "userRandomID": {
-    id: "userRandomID", 
-    email: "user@example.com", 
+{
+  "userRandomID":
+  {
+    id: "userRandomID",
+    email: "user@example.com",
     password: "purple-monkey-dinosaur"
   },
- "user2RandomID": {
-    id: "user2RandomID", 
-    email: "user2@example.com", 
+  "user2RandomID":
+  {
+    id: "user2RandomID",
+    email: "user2@example.com",
     password: "dishwasher-funk"
   }
 }
 
-app.get("/", (request, response) =>
-{
+app.get("/", (request, response) => {
   response.send("Hello!");
 });
 
-app.get("/hello", (request, response) =>
-{
+app.get("/hello", (request, response) => {
   response.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
-app.get("/urls", (request, response) =>
-{
-  const template = { urls: database, username: request.cookies["username"], };
+app.get("/urls", (request, response) => {
+  const template = { urls: urlDatabase, username: request.cookies["username"], };
   response.render("Index", template);
 });
 
-app.post("/urls", (request,response) =>
-{
-  console.log(request.body);
-  response.send("Ok");
-}); //request.body = information from forms
-//request.params = information from  url
-
-app.get("/urls/new", (request, response) =>
-{
-  const template = { urls: database, username: request.cookies["username"], };
-  response.render("New", template);
+app.post("/urls", (request, response) => {
+  console.log(request.body.longURL);
+  let longURL = request.body.longURL;
+  urlDatabase[longURL];
+  const template = { urls: urlDatabase, username: request.cookies["username"], };
+  const urlContent = request.body.urlContent;
+  const urlID = Math.random().toString(36).slice(2, 8);
+  const newUrl =
+  {
+    id: urlID,
+    shortURL: urlContent
+  }
+  urlDatabase[urlID] = newUrl;
+  response.redirect("/urls");
 });
 
-app.get("/urls/:shortURL", (request, response) =>
-{
-  const template = { shortURL: request.params.shortURL, longURL: database[request.params.shortURL] };
+//request.body = information from forms
+//request.params = information from  url
+
+app.get("/urls/new", (request, response) => {
+  const template = { urls: urlDatabase, username: request.cookies["username"], };
+  response.render("New");
+});
+
+app.get("/urls/:shortURL", (request, response) => {
+  const template = { shortURL: request.params.shortURL, longURL: urlDatabase[request.params.shortURL], username: request.cookies["username"] };
   //("/urls/:shortURL" === shortURL: request.params.shortURL
   response.render("Show", template);
 });
 
-app.get("/u/:shortURL", (request, response) =>
-{
+app.get("/u/:shortURL", (request, response) => {
   response.redirect(longURL);
 });
 
-app.get("/urls/:shortURL/show", (request, response) =>
-{
+app.get("/urls/:shortURL/show", (request, response) => {
   response.redirect("/urls/:shortURL");
   response.redirect("/urls");
 }); //handler = post or get
 //post => SEND DATA
 
-app.post("/urls/:shortURL/delete", (request, response) =>
-{
-  delete database[request.params.shortURL];
+app.post("/urls/:shortURL/delete", (request, response) => {
+  delete urlDatabase[request.params.shortURL];
   response.redirect("/urls");
 });
 //:variable = for express
 //ALWAYS console.log response(body)/response.body and/or request.params
 
-app.post("/urls/register", (request, response) =>
-{
-  response.cookie("userID");
-/*
-<form method="POST" <action="/urls/register>
-<input type="email" name="email">
-<input type="password" name="password">
-<button type="submit">Register</button>
-*/
-  response.redirect("/urls");
-});
-
-app.get("/urls/register", (request, response) =>
-{
+app.get("/urls/register", (request, response) => {
   response.cookie("email");
-  const template = {email: request.cookies["email"]};
-  
+  const template = { email: request.cookies["email"] };
+
   response.render("Register", template);
 });
 
-app.post("/urls/login", (request, response) =>
-{
-  response.cookie("username", request.body.username);
-  console.log(request.body);
-  const template = {username: request.cookies["username"], urls: database};
+app.post("/urls/register", (request, response) => {
+  response.cookie("userID");
+  if (email === "" || password === "" || email === users[email]) { response.send("Error code 400"); };
+  /*
+  */
   response.redirect("/urls");
 });
 
-app.post("/urls/logout", (request, response) =>
-{
+app.get("/urls/login", (request, response) => {
+
+});
+
+app.post("/urls/login", (request, response) => {
+  /*
+      <% if (!username) { %>
+    <form method="POST" action="/urls/login"><input type="text" name="username" placeholder="Username">
+      <button type="submit">Submit</button>
+    </form>
+  <% } else { %>
+    Logged in as: <%= username %><form method="POST" action="/urls/logout"><button type="submit">Logout</button></form>
+    <% } %>
+  */
+  if (email !== users[email] || email === users[email] && password !== users[password]) { response.send("Error code 403"); }
+  else if (email === users[email] && password === users[password]) {
+    response.cookie("userID");
+    response.redirect("/urls");
+  };
+  response.cookie("username", request.body.username);
+  console.log(request.body);
+  const template = { username: request.cookies["username"], urls: urlDatabase };
+  response.redirect("/urls");
+});
+
+app.post("/urls/logout", (request, response) => {
   response.clearCookie("username");
   response.redirect("/urls");
 });
 
-app.listen(PORT, () =>
-{
+app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}!`);
 });
