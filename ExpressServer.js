487a4944463807a7f0abb12495e88dea0da5eb0b
MM0nty <79332342+MM0nty@users.noreply.github.com>
2021-08-12 19:34:52 +0000

    Fix all code (only users can edit, delete and see URLs they create, index is now correctly showing the edited longURL of a shortURL, trying to login with empty email or password is now fixed and shows relevant HTML message, register button is now only seen while logged out) with a mentor

--- ExpressServer.js
+++ ExpressServer.js
@@ -4,7 +4,7 @@ const PORT = 8080;
 const bodyParser = require("body-parser");
 const bcrypt = require("bcryptjs");
 const cookieSession = require("cookie-session");
-const { userDatabase, findUser, verify } = require("./Helpers");
+const { userDatabase, urlDatabase, findUser, verify, userURLs } = require("./Helpers");
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieSession({
@@ -13,17 +13,6 @@ app.use(cookieSession({
 }));
 
 app.set("view engine", "ejs");
-
-const urlDatabase = {
-  "b2xVn2": {
-    longURL: "https://www.lighthouselabs.ca",
-    userID: "userRandomID"
-  },
-  "9sm5xK": {
-    longURL: "https://www.google.com",
-    userID: "user2RandomID"
-  }
-};
 
 app.get("/", (request, response) => {
   response.send("Hello!");
@@ -37,29 +26,26 @@ app.get("/hello", (request, response) => {
   response.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
-//show index page
 app.get("/urls", (request, response) => {
-  const template = { urls: urlDatabase, email: request.session.email };
-  console.log(request.session.email);
-  response.render("Index", template);
+  const userID = request.session.userID;
+  const template = {urls: userURLs(userID), email: request.session.email };
+    response.render("Index", template);
 });
 
+//Takes in longURL, creates userID cookie, and generates random string for urlID to make newURL object and adding it to urlDatabase
 app.post("/urls", (request, response) => {
-  console.log(request.body);
   const longURL = request.body.longURL;
   const userID = request.session.userID;
   const urlID = Math.random().toString(36).slice(2, 8);
   const newURL = { urlID, longURL, userID };
   urlDatabase[urlID] = newURL;
-  console.log(urlDatabase);
-  response.redirect("/urls/:shortURL");
+  response.redirect("/urls/" + urlID);
 });
 
-//Create new URL
+//Redirects to login if not logged in
 app.get("/urls/new", (request, response) => {
   const template = { urls: urlDatabase, email: request.session.email };
   if (!request.session.email) {
-    console.log("Error code 403. Please login to make new tiny URLs");
     response.redirect("/login");
   }
   response.render("New", template);
@@ -79,23 +65,21 @@ app.get("/u/:shortURL", (request, response) => {
 app.post("/urls/:shortURL", (request, response) => {
   const shortURL = request.params.shortURL;
   const longURL = request.body.longURL;
-  urlDatabase[shortURL] = longURL;
+  urlDatabase[shortURL].longURL = longURL;
   response.redirect("/urls");
 });
 
-//deleting an URL
 app.post("/urls/:shortURL/delete", (request, response) => {
   delete urlDatabase[request.params.shortURL];
   response.redirect("/urls");
 });
 
-//show register page
 app.get("/register", (request, response) => {
   const template = { email: request.session.email };
   response.render("Register", template);
 });
 
-//registering
+//Registers user in userDatabase if email is not already used. Hashes password and encrypts userID for security
 app.post("/register", (request, response) => {
   const { email, password } = request.body;
   const user = findUser(userDatabase, email);
@@ -109,28 +93,26 @@ app.post("/register", (request, response) => {
   const hashed = bcrypt.hashSync(password, salt);
   const newUser = { userID, email, password: hashed };
   userDatabase[userID] = newUser;
-  console.log(userDatabase);
   response.redirect("/urls");
 });
 
-//show login page
 app.get("/login", (request, response) => {
   const template = { email: request.session.email };
   response.render("Login", template);
 });
 
-//logging in
+//Verifies if email and password are correct
 app.post("/login", (request, response) => {
   const { email, password } = request.body;
   const user = verify(email, password);
   if (!user) {
-    response.status(403).send("Error code 403");
+    response.status(403).send("Login failed. Error code 403");
   }
   request.session.email = email;
   response.redirect("/urls");
 });
 
-//clicking logout
+//Deletes cookies
 app.post("/logout", (request, response) => {
   request.session = null;
   response.redirect("/urls");
--- Helpers.js
+++ Helpers.js
@@ -13,7 +13,18 @@ const userDatabase = {
   }
 };
 
-//find if email already used
+const urlDatabase = {
+  "b2xVn2": {
+    longURL: "https://www.lighthouselabs.ca",
+    userID: "userRandomID"
+  },
+  "9sm5xK": {
+    longURL: "https://www.google.com",
+    userID: "user2RandomID"
+  }
+};
+
+//Takes in email and looks through userDatabase to find if it's already used, then returns it. If not, it returns null
 const findUser = function(userDatabase, email) {
   for (const user in userDatabase) {
     if (email === userDatabase[user].email) {
@@ -23,16 +34,24 @@ const findUser = function(userDatabase, email) {
   return null;
 };
 
-//verify login
+//Takes in email and password and returns valid user
 const verify = (email, password) => {
   const user = findUser(userDatabase, email);
-  const match = bcrypt.compareSync(password, user.password);
-  console.log("Password ", password);
-  console.log("User.password ", user.password);
-  if (user && match === true) {
+  if (user && bcrypt.compareSync(password, user.password)) {
     return user;
   }
   return undefined;
 };
 
-module.exports = { userDatabase, findUser, verify };
+//Takes in userID, looks through urlDatabase and returns URLs with your userID
+const userURLs = function(userID) {
+  let filteredUrls = {};
+  for (urlID in urlDatabase) {
+    if (urlDatabase[urlID].userID === userID) {
+      filteredUrls[urlID] = urlDatabase[urlID];
+    }
+  }
+  return filteredUrls;
+}
+
+module.exports = { userDatabase, urlDatabase, findUser, verify, userURLs };
