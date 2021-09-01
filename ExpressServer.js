595c45b9713d3dc0fb8dca19d3d567266a6bd01b
MM0nty <79332342+MM0nty@users.noreply.github.com>
2021-09-01 18:53:24 +0000

    Will allow the user to register an account

--- ExpressServer.js
+++ ExpressServer.js
@@ -15,6 +15,19 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
+const userDatabase = {
+  "userRandomID": {
+    userID: "userRandomID",
+    email: "user@example.com",
+    password: "purple-monkey-dinosaur"
+  },
+  "user2RandomID": {
+    userID: "user2RandomID",
+    email: "user2@example.com",
+    password: "dishwasher-funk"
+  }
+};
+
 app.get("/", (request, response) => {
   response.send("Hello!");
 });
@@ -28,7 +41,7 @@ app.get("/hello", (request, response) => {
 });
 
 app.get("/urls", (request, response) => {
-  const template = { urls: urlDatabase, username: request.cookies["username"] };
+  const template = { urls: urlDatabase, email: request.cookies["email"]};
   response.render("Index", template);
 });
 
@@ -40,7 +53,7 @@ app.post("/urls", (request, response) => {
 });
 
 app.get("/urls/new", (request, response) => {
-  const template = { username: request.cookies["username"]}
+  const template = { email: request.cookies["email"]};
   response.render("New", template);
 });
 
@@ -51,7 +64,7 @@ app.get("/u/:shortURL", (request, response) => {
 });
 
 app.get("/urls/:shortURL", (request, response) => {
-  const template = { shortURL: request.params.shortURL, longURL: urlDatabase[request.params.shortURL]/* What goes here? */, username: request.cookies["username"] };
+  const template = { shortURL: request.params.shortURL, longURL: urlDatabase[request.params.shortURL]/* What goes here? */, email: request.cookies["email"]};
   response.render("Show", template);
 });
 
@@ -59,7 +72,7 @@ app.post("/urls/:shortURL", (request, response) => {
   const shortURL = request.params.shortURL;
   const longURL = request.body.longURL;
   urlDatabase[shortURL] = longURL;
-  const template = { username: request.cookies["username"]}
+  const template = { email: request.cookies["email"]};
   response.render("Index", template);
 });
 
@@ -68,14 +81,35 @@ app.post("/urls/:shortURL/delete", (request, response) => {
   response.render("Index", template);
 });
 
+app.get("/register", (request, response) => {
+  const template = { email: request.cookies["email"]};
+  // const template = { email: request.session.email };
+  response.render("Register", template);
+});
+
+app.post("/register", (request, response) => {
+  const { email, password } = request.body;
+  // const user = findUser(userDatabase, email);
+  if (email === "" || password === "") {//} || user) {
+    response.status(400).send("Registration failed. Error code 400");
+  }
+  const userID = Math.random().toString(36).slice(2, 8);
+  const newUser = { userID, email, password };
+  userDatabase[userID] = newUser;
+  response.cookie("email", email);
+  response.cookie("userID", userID);
+  console.log("userDatabase:", userDatabase);
+  response.redirect("/urls");
+});
+
 app.post("/login", (request, response) => {
-  const username = request.body.username;
-  response.cookie("username", username);
+  const email = request.body.email;
+  response.cookie("email", email);
   response.redirect("/urls");
 })
 
 app.post("/logout", (request, response) => {
-  response.clearCookie("username");
+  response.clearCookie("email");
   response.redirect("/urls");
 });
 
