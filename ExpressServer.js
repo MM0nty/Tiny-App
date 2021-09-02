93a67333aff6b19ce42f34060acdbcdab583e39f
MM0nty <79332342+MM0nty@users.noreply.github.com>
2021-09-02 19:49:09 +0000

    Allow only the creators of URLs to delete them

--- ExpressServer.js
+++ ExpressServer.js
@@ -1,19 +1,17 @@
 const express = require("express");
 const app = express();
 const PORT = 8080;
+const bodyParser = require("body-parser");
 const bcrypt = require("bcryptjs");
 const cookieSession = require("cookie-session");
 const { userDatabase, urlDatabase, findUser, userURLs } = require("./Helpers");
 
 app.set("view engine", "ejs");
-const bodyParser = require("body-parser");
-const { request, response } = require("express");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieSession({
   name: "session",
   keys: ["key0"]
 }));
-
 
 app.get("/", (request, response) => {
   response.send("Hello!");
@@ -33,6 +31,7 @@ app.get("/urls", (request, response) => {
   response.render("Index", template);
 });
 
+//Takes in longURL, creates userID cookie, and generates random string for shortURL to make newURL object and adding it to urlDatabase
 app.post("/urls", (request, response) => {
   const longURL = request.body.longURL;
   const userID = request.session.userID;
@@ -52,30 +51,35 @@ app.get("/urls/new", (request, response) => {
 
 app.get("/u/:shortURL", (request, response) => {
   const shortURL = request.params.shortURL;
-  const longURL = urlDatabase[request.params.shortURL].longURL;
+  const longURL = urlDatabase[shortURL].longURL;
   response.redirect(longURL);
 });
 
 app.get("/urls/:shortURL", (request, response) => {
-  const template = { shortURL: request.params.shortURL, longURL: urlDatabase[request.params.shortURL].longURL, email: request.session.email };
+  const userID = request.session.userID;
+  const template = { shortURL: request.params.shortURL, longURL: userURLs(userID)[request.params.shortURL].longURL, email: request.session.email };
+  console.log("urlDatabase:", urlDatabase);
   response.render("Show", template);
 });
 
 app.post("/urls/:shortURL", (request, response) => {
+  const userID = request.session.userID;
   const shortURL = request.params.shortURL;
   const longURL = request.body.longURL;
-  urlDatabase[shortURL] = longURL;
-  const template = { email: request.session.email };
+  console.log("urlDatabase:", urlDatabase);
+  urlDatabase[shortURL].longURL = longURL;
+  console.log("urlDatabase:", urlDatabase);
+  const template = { urls: userURLs(userID), email: request.session.email };
   response.render("Index", template);
 });
 
 app.post("/urls/:shortURL/delete", (request, response) => {
-  const template = { email: request.session.email };
-  if (request.session.email) {
-    //Fix deleting (then editing)
+  const userID = request.session.userID;
+  const template = { urls: userURLs(userID), email: request.session.email };
+  if (request.session.email && userID === urlDatabase[request.params.shortURL].userID) {
     delete urlDatabase[request.params.shortURL];
   }
-  response.render("Index", template);
+  response.redirect("/urls");
 });
 
 app.get("/register", (request, response) => {
@@ -83,6 +87,7 @@ app.get("/register", (request, response) => {
   response.render("Register", template);
 });
 
+//Registers user in userDatabase if email is not already used
 app.post("/register", (request, response) => {
   const { email, password } = request.body;
   const user = findUser(userDatabase, email);
@@ -90,11 +95,11 @@ app.post("/register", (request, response) => {
     response.redirect(400, "/register");
   }
   const userID = Math.random().toString(36).slice(2, 8);
+  //Hashes password and encrypts cookies for security
   const salt = bcrypt.genSaltSync(10);
   const hashed = bcrypt.hashSync(password, salt);
   const newUser = { userID, email, password: hashed };
   userDatabase[userID] = newUser;
-  console.log("userDatabase:", userDatabase);
   request.session.email = email;
   request.session.userID = userID;
   response.redirect("/urls");
@@ -108,6 +113,7 @@ app.get("/login", (request, response) => {
 app.post("/login", (request, response) => {
   const { email, password } = request.body;
   const user = findUser(userDatabase, email);
+  //If email and password are correct, redirects to /urls
   if (user && bcrypt.compareSync(password, user.password)) {
     request.session.email = email;
     request.session.userID = user.userID;
@@ -116,11 +122,12 @@ app.post("/login", (request, response) => {
   return response.redirect(403, "/login");
 })
 
+//Deletes all cookies
 app.post("/logout", (request, response) => {
   request.session = null;
   response.redirect("/urls");
 });
 
 app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}!`);
+  console.log(`Listening on port ${PORT}!`);
 });
